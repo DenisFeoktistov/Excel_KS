11844bd7571f0fcecc3f1e63623ae0efd9198c7d
DenisFeoktistov <dfeoktistov523@icloud.com>
2022-07-27 16:52:38 +0500

    Some changes with interface

--- App/Classes/MainWindow.py
+++ App/Classes/MainWindow.py
@@ -8,12 +8,17 @@ import App.App as App
 import App.Classes.Action as Action
 
 
-FILE_SELECT_TEXT = ""
+"❌"
+"✔"
+
+
+class Color:
+    main2 = "rgb(211, 211, 211)"
+    main1 = "rgb(231, 231, 231)"
 
 
 class MainWindow(QMainWindow):
     REL_WIDTH, REL_HEIGHT = 0.3, 0.3
-
     def __init__(self, app: App.App) -> None:
         super().__init__()
 
@@ -25,7 +30,7 @@ class MainWindow(QMainWindow):
 
         user_screen_geometry = self.app.desktop().screenGeometry()
         self.width = 450  # int(user_screen_geometry.width() * MainWindow.REL_WIDTH)
-        self.height = 250  # int(user_screen_geometry.height() * MainWindow.REL_HEIGHT)
+        self.height = 300  # int(user_screen_geometry.height() * MainWindow.REL_HEIGHT)
 
         # self.file_scroll_area = FileScrollArea(self)
         # self.info_scroll_area = InfoScrollArea(self)
@@ -34,7 +39,11 @@ class MainWindow(QMainWindow):
         # self.info_scroll_area.add_widget(self.info_label)
 
         self.file_select_button = QPushButton(self)
+        self.file_select_label = QLabel(self)
+
         self.output_file_select_button = QPushButton(self)
+        self.output_file_select_label = QLabel(self)
+
         self.enter_button = QPushButton(self)
 
         self.labels = list()
@@ -50,7 +59,10 @@ class MainWindow(QMainWindow):
         # self.set_up_info_scroll_area()
 
         self.set_up_file_select_button()
+        self.set_up_file_select_label()
+
         self.set_up_output_file_select_button()
+        self.set_up_output_file_select_label()
         self.set_up_enter_button()
 
     def set_up_window(self) -> None:
@@ -68,155 +80,83 @@ class MainWindow(QMainWindow):
 
         rel_width = 0.79
         rel_height = 0.32
-        self.file_select_button.setFixedSize(int(self.width * rel_width), int(self.height * rel_height))
-        self.file_select_button.move(int(self.width * 0.01), int(self.height * 0.01))
+        self.file_select_button.setFixedSize(330, 70)
+        self.file_select_button.move(5, 5)
 
         self.file_select_button.setStyleSheet(
-            "border: 2px solid black; border-radius: 5px; font-size: 20px; font-weight: 200; background: rgb(235, 71, 0); color: black;")
+            f"""
+            QPushButton {{
+                border: 2px solid black; 
+                border-radius: 5px; 
+                font-size: 20px; 
+                font-weight: 200; 
+                background: {Color.main1};
+            }}
+            QPushButton:hover {{
+                background: {Color.main2};
+            }}
+            """
+        )
 
     def set_up_output_file_select_button(self):
         self.output_file_select_button.setText("Выберите папку, в которую\n необходимо загрузить результат")
 
-        rel_width = 0.79
-        rel_height = 0.32
-        self.output_file_select_button.setFixedSize(int(self.width * rel_width), int(self.height * rel_height))
-        self.output_file_select_button.move(int(self.width * 0.01), int(self.height * 0.34))
+        self.output_file_select_button.setFixedSize(330, 70)
+        self.output_file_select_button.move(5, 100)
 
         self.output_file_select_button.setStyleSheet(
-            "border: 2px solid black; border-radius: 5px; font-size: 20px; font-weight: 200; background: rgb(235, 71, 0); color: black;")
+            f"""
+            QPushButton {{
+                border: 2px solid black; 
+                border-radius: 5px; 
+                font-size: 20px; 
+                font-weight: 200; 
+                background: {Color.main1};
+            }}
+            QPushButton:hover {{
+                background: {Color.main2};
+            }}
+            """
+        )
 
     def set_up_enter_button(self):
         self.enter_button.setText("Обработать")
 
-        rel_width = 0.5
-        rel_height = 0.30
-        self.enter_button.setFixedSize(int(self.width * rel_width), int(self.height * rel_height))
-        self.enter_button.move(int(self.width / 2 - self.width * rel_width / 2), int(self.height * 0.67))
+        self.enter_button.setFixedSize(200, 80)
+        self.enter_button.move(125, 200)
 
         self.enter_button.setStyleSheet(
-            "border: 2px solid black; border-radius: 5px; font-size: 20px; font-weight: 200; background: rgb(235, 71, 0); color: black;")
+            f"""
+            QPushButton {{
+                border: 2px solid black; 
+                border-radius: 5px; 
+                font-size: 20px; 
+                font-weight: 200; 
+                background: {Color.main1};
+            }}
+            QPushButton:hover {{
+                background: {Color.main2};
+            }}
+            """
+        )
 
-    # def set_up_file_scroll_area(self):
-    #     rel_width = 0.98
-    #     rel_height = 0.48
-    #
-    #     self.file_scroll_area.setFixedSize(int(self.width * rel_width), int(self.height * rel_height))
-    #     self.file_scroll_area.move(int(self.width * 0.01), int(self.height * 0.01))
-    #     self.file_scroll_area.setWidgetResizable(True)
-    #
-    #     self.file_scroll_area.setStyleSheet("border: 2px solid black; border-radius: 5px")
-    #     self.file_scroll_area.verticalScrollBar().setStyleSheet("""
-    #                                     QScrollBar {
-    #                                         background: rgb(170, 170, 170);
-    #                                         border: 0px solid black;
-    #                                     }
-    #
-    #                                     QScrollBar::add-line {
-    #                                         background: red;
-    #                                     }
-    #
-    #                                     QScrollBar::sub-line:vertical {
-    #                                         background: red;
-    #                                     }
-    #
-    #                                     QScrollBar::handle {
-    #                                         background: rgb(235, 71, 0);
-    #                                         border: 2px solid rgb(70, 70, 70);
-    #                                         border-radius: 3px;
-    #                                     }
-    #                                     """)
-    #     self.file_scroll_area.verticalScrollBar().setFixedWidth(15)
-    #     self.file_scroll_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-    #
-    #     self.file_scroll_area.setViewportMargins(int(self.width * 0.01), 0, int(self.width * 0.01), 0)
-    #
-    #     for i in range(30):
-    #         self.labels.append(QLabel(parent=self, text="2 ТП-3083 ЗРЭС.xlsx"))
-    #         self.file_scroll_area.add_widget(self.labels[-1])
+    def set_up_file_select_label(self):
+        self.file_select_label.setText("❌")
 
-    # def set_up_info_scroll_area(self):
-    #     rel_width = 0.48
-    #     rel_height = 0.98
-    #
-    #     self.info_scroll_area.setFixedSize(int(self.width * rel_width), int(self.height * rel_height))
-    #     self.info_scroll_area.move(int(self.width * 0.01), int(self.height * 0.01))
-    #     self.info_scroll_area.setWidgetResizable(True)
-    #
-    #     self.info_scroll_area.setStyleSheet("border: 2px solid black; border-radius: 5px")
-    #     self.info_scroll_area.verticalScrollBar().setStyleSheet("""
-    #                                             QScrollBar {
-    #                                                 background: rgb(170, 170, 170);
-    #                                                 border: 0px solid black;
-    #                                             }
-    #
-    #                                             QScrollBar::add-line {
-    #                                                 background: red;
-    #                                             }
-    #
-    #                                             QScrollBar::sub-line:vertical {
-    #                                                 background: red;
-    #                                             }
-    #
-    #                                             QScrollBar::handle {
-    #                                                 background: rgb(235, 71, 0);
-    #                                                 border: 2px solid rgb(70, 70, 70);
-    #                                                 border-radius: 3px;
-    #                                             }
-    #                                             """)
-    #     self.info_scroll_area.verticalScrollBar().setFixedWidth(int(self.width * 0.02))
-    #     self.info_scroll_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-    #
-    #     self.info_scroll_area.setViewportMargins(int(self.width * 0.01), 0, int(self.width * 0.01), 0)
+        self.file_select_label.setFixedSize(70, 70)
+        self.file_select_label.move(370, 5)
 
+        self.file_select_label.setStyleSheet(
+            f"border: 1px solid black; border-radius: 5px; font-size: 30px; font-weight: 200; background: {Color.main1}; color: red;")
+        self.file_select_label.setAlignment(QtCore.Qt.AlignCenter)
 
-# class ScrollAreaMain(QScrollArea):
-#     def __init__(self, parent: QMainWindow) -> None:
-#         super().__init__(parent=parent)
-#         self.container = Container()
-#         self.setWidget(self.container)
-#
-#     def add_widget(self, widget: QWidget) -> None:
-#         self.container.add_widget(widget)
-#
-#     def clear(self) -> None:
-#         self.container.clear()
+    def set_up_output_file_select_label(self):
+        self.output_file_select_label.setText("✔")
 
+        self.output_file_select_label.setFixedSize(70, 70)
+        self.output_file_select_label.move(370, 100)
 
-# class FileScrollArea(ScrollAreaMain):
-#     def set_up_widget(self, widget: QLabel) -> None:
-#         widget.setFixedHeight(30)
-#         widget.setFixedWidth(int((self.width() - self.verticalScrollBar().width()) * 0.93))
-#         widget.setStyleSheet(
-#             "border: 1px solid black; border-radius: 5px; font-size: 18px; font-weight: 300; background: rgb(51, 204, 51); color: black;")
-#         widget.setAlignment(QtCore.Qt.AlignCenter)
-#
-#     def add_widget(self, widget: QLabel) -> None:
-#         super(FileScrollArea, self).add_widget(widget)
-#         self.set_up_widget(widget)
+        self.output_file_select_label.setStyleSheet(
+            f"border: 1px solid black; border-radius: 5px; font-size: 30px; font-weight: 200; background: {Color.main1}; color: green;")
+        self.output_file_select_label.setAlignment(QtCore.Qt.AlignCenter)
 
-
-
-# class InfoScrollArea(ScrollAreaMain):
-#     def set_up_widget(self, widget: QWidget) -> None:
-#         widget.setFixedHeight(22)
-#         widget.setFixedWidth(int(self.width() * 0.75))
-#         widget.setStyleSheet(
-#             "border: 1px solid black; border-radius: 5px; font-size: 13px; font-weight: 300; background: rgb(51, 204, 51); color: black;")
-#
-#     def add_widget(self, widget: QWidget) -> None:
-#         super(InfoScrollArea, self).add_widget(widget)
-#         self.set_up_widget(widget)
-
-
-# class Container(QWidget):
-#     def __init__(self) -> None:
-#         super().__init__()
-#         self.setLayout(QVBoxLayout())
-#
-#     def add_widget(self, widget: QWidget) -> None:
-#         widget.setParent(self)
-#         self.layout().addWidget(widget)
-#
-#     def clear(self) -> None:
-#         for i in reversed(range(self.layout().count())):
-#             self.layout().itemAt(i).widget().setParent(None)
